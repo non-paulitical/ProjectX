07ea3d31762ecbc2abe2d06f91e3f171ba875f44
Abhijit <paulabhijit2607@gmail.com>
2025-03-19 09:15:01 +0530

    Modified the header with regards to the sidebar.

--- frontend/client/src/components/Header.tsx
+++ frontend/client/src/components/Header.tsx
@@ -2,15 +2,12 @@ import { NotebookPen } from 'lucide-react';
 
 const Header = () => {
   return (
-    <header className="flex justify-between items-center sticky">
-      <section>
-        Sidebar Icon
-      </section>
-      <section className="font-semibold text-4xl">
+    <header className=" flex-1 flex items-center justify-between">
+      <section className="font-semibold text-2xl md:text-3xl lg:text-4xl">
         Project X
       </section>
       <section>
-        <NotebookPen className='cursor-pointer'/>
+        <NotebookPen className='cursor-pointer' />
       </section>
     </header>
   )
