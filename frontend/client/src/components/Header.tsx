944b69fcbe5bbe5eec41bbd3a451f82bfd202325
Abhijit <paulabhijit2607@gmail.com>
2025-03-25 11:47:58 +0530

    Navigation and Styling to the Header
    The NotebookPen icon is used to navigate to the /app path to create new
    spaces. And styling accoringly to disable navigation when the url path
    is /app.

--- frontend/client/src/components/Header.tsx
+++ frontend/client/src/components/Header.tsx
@@ -1,13 +1,19 @@
-import { NotebookPen } from 'lucide-react';
+import { NotebookPen } from 'lucide-react'
+import { useLocation, Link } from 'react-router'
 
 const Header = () => {
+  const location = useLocation()
+
   return (
     <header className=" flex-1 flex items-center justify-between">
       <section className="font-semibold text-2xl md:text-3xl lg:text-4xl">
         Project X
       </section>
       <section>
-        <NotebookPen className='cursor-pointer' />
+        { location.pathname.replace(/\/$/, '') === '/app' ? <NotebookPen className='filter-big brightness-50 cursor-not-allowed' /> :
+         <Link to='/app'>
+          <NotebookPen className='cursor-pointer' />
+        </Link>}
       </section>
     </header>
   )
